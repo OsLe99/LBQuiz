d99f68cce87b9dd6ce910eb76fd48f4d26e7536d
Oscar Lejon <oscar.lejon@campusnykoping.se>
2026-01-29 16:20:06 +0100

    Added lobby-model

---
+++ LBQuiz/Models/Lobby/QuizLobby.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace LBQuiz.Models.Lobby;
+
+public class QuizLobby
+{
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "QuizId is required")]
+    public int QuizId { get; set; }
+
+    [Required(ErrorMessage = "JoinCode is required")]
+    [StringLength(6, MinimumLength = 4, ErrorMessage = "QuizCode must be between 4 and 6 characters")]
+    public string JoinCode { get; set; }
+    
+    [Required(ErrorMessage = "Quiz creation date is required")]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    
+    [Required(ErrorMessage = "Quiz bool statement is required")]
+    public bool IsActive { get; set; } = true;
+
+    public QuizLobby()
+    {
+        JoinCode = GenerateJoinCode();
+    }
+
+    private string GenerateJoinCode(int length = 6)
+    {
+        const string validChars = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
+        var random = new Random();
+        return new string(Enumerable.Repeat(validChars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+
+    public static bool IsValidJoinCode(string code)
+    {
+        return !string.IsNullOrEmpty(code) && 
+               code.Length >= 4 &&
+               code.Length <= 6 &&
+               code.All(c => "ABCDEFGHJKLMNPQRSTUVWXYZ23456789".Contains(char.ToUpper(c)));
+    }
+}
