69b5349192c303949a00d178842bba2066eb6534
Oscar Lejon <oscar.lejon@campusnykoping.se>
2026-01-29 08:18:22 +0100

    Added form for adding questions

---
+++ LBQuiz/Migrations/20260128150718_UpdatedQuiz.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LBQuiz.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedQuiz : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Quiz",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Quiz",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Quiz");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Quiz");
+        }
+    }
+}
