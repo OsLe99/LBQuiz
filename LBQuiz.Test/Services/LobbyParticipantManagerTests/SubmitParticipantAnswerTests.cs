1a6ef0ab59547deec4c9ae813920c3c8410d3812
Oscar Lejon <oscar.lejon@campusnykoping.se>
2026-02-10 13:23:41 +0100

    Commented out unused method. No use, no test

---
+++ LBQuiz.Test/Services/LobbyParticipantManagerTests/SubmitParticipantAnswerTests.cs
@@ -0,0 +1,35 @@
+﻿using LBQuiz.Services;
+using LBQuiz.Models.Lobby;
+
+namespace LBQuiz.Test.Services.LobbyParticipantManagerTests;
+
+public class SubmitParticipantAnswerTests
+{
+    // Implement when we actually use this method
+    // [Fact]
+    // public async Task SubmitParticipantAnswer_MultipleQuestions_ShouldStoreALlAnswers()
+    // {
+    //     // Arrange
+    //     var manager = new LobbyParticipantManager();
+    //     var participant = new LobbyParticipant
+    //     {
+    //         ConnectionId = "newCon-123",
+    //         Nickname = "TestUser1"
+    //     };
+    //     manager.AddParticipant(1, participant);
+    //     
+    //     // Act
+    //     await manager.SubmitParticipantAnswer("newCon-123", "Answer1", 1);
+    //     await manager.SubmitParticipantAnswer("newCon-123", "Answer2", 2);
+    //     await manager.SubmitParticipantAnswer("newCon-123", "Answer3", 3);
+    //     
+    //     var result1 = await manager.CheckAnswer("Answer1", 1,"newCon-123" );
+    //     var result2 = await manager.CheckAnswer("Answer2", 2, "newCon-123" );
+    //     var result3 = await manager.CheckAnswer("Answer3", 3, "newCon-123" );
+    //     
+    //     // Assert
+    //     Assert.True(result1);
+    //     Assert.True(result2);
+    //     Assert.True(result3);
+    // }
+}
