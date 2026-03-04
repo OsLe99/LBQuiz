79e2ffe772c87fcc524a4eef0b0e05821b37d177
Oscar Lejon <oscar.lejon@campusnykoping.se>
2026-03-04 11:58:32 +0100

    Added back cookiebanner and added new theme

---
+++ LBQuiz/Themes/LBQuizCustomTheme.cs
@@ -0,0 +1,28 @@
+﻿using MudBlazor;
+
+namespace LBQuiz.Themes;
+
+public static class LBQuizCustomTheme
+{
+    public static class LBQuizTheme
+    {
+        public static MudTheme Instance { get;  } = new MudTheme
+        {
+            PaletteLight = new PaletteLight()
+            {
+                Primary = "#FF8C00",
+                Secondary = "#1A237E",
+                Background = "#F5F5F5",
+                Surface = "#FFFFFF",
+                AppbarBackground = "#FF8C00",
+                AppbarText = "#FFFFFF",
+                DrawerBackground = "#FFFFFF",
+                DrawerText = "#000000",
+                Success = "#4CAF50",
+                Info = "#2196F3",
+                Warning = "#FFC107",
+                Error = "#F44336"
+            }
+        };
+    }
+}
