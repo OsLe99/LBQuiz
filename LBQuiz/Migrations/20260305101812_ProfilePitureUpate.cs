addf81bb3587dd8e26ac25527d332b1e34b75e7a
Emil Berg <emil94berg@gmail.com>
2026-03-05 11:56:34 +0100

    profile picture

---
+++ LBQuiz/Migrations/20260305101812_ProfilePitureUpate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LBQuiz.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProfilePitureUpate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ProfilePicture",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ProfilePicture",
+                table: "AspNetUsers",
+                type: "varbinary(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
